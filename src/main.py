e2d8db8ad33ab1e8e64d9999a8e385ab7cf8f432
PHRaulino <54751775+PHRaulino@users.noreply.github.com>
2023-04-18 11:47:28 -0300

    feat(main): Adicionado FastAPI e rotas Favicon e Hello World, configurado VSCode para feedback rápido
    Neste commit, eu adicionei o módulo principal do FastAPI à aplicação e criei duas rotas, uma para o Favicon e outra para a mensagem "Hello World" na raiz da aplicação. Também configurei o VSCode para que eu possa executar a aplicação localmente fora do Docker, permitindo um feedback mais rápido durante o desenvolvimento.

--- src/main.py
+++ src/main.py
@@ -1,1 +1,15 @@
-print("hello world")
+from fastapi import FastAPI
+from starlette.responses import FileResponse
+
+app = FastAPI()
+favicon_path = "assets/favicon.ico"
+
+
+@app.get("/favicon.ico")
+async def favicon():
+    return FileResponse(favicon_path)
+
+
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
