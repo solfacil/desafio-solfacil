fa99f8d4f4917d2acd38a04604f6dbab85c68ad4
PHRaulino <54751775+PHRaulino@users.noreply.github.com>
2023-04-18 23:57:38 -0300

    feat(app): Adicionado get_db como dependência para instanciar conexão com o banco nas rotas da API
    Neste commit, foi adicionado o método get_db como dependência nas rotas da API. Esse método é responsável por instanciar a conexão com o banco de dados por meio da biblioteca SQLAlchemy e fornecer essa conexão para as rotas que necessitam dela. Dessa forma, é possível manter uma única conexão durante toda a requisição, garantindo um melhor desempenho e evitando possíveis problemas de concorrência no acesso ao banco de dados.

--- src/main.py
+++ src/main.py
@@ -1,8 +1,18 @@
 from fastapi import FastAPI
 from starlette.responses import FileResponse
 
+from src.database import SessionLocal
+
 app = FastAPI()
 favicon_path = "assets/favicon.ico"
+
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
 
 
 @app.get("/favicon.ico")
