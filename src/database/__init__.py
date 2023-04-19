c39e0a5d082e47636f1e16f5e1001f372caa430d
PHRaulino <54751775+PHRaulino@users.noreply.github.com>
2023-04-19 00:14:26 -0300

    refactor(database): Alterando a importação do declarative_base() que agora está disponível no módulo sqlalchemy.orm ao invés de sqlalchemy.ext.declarative
    Esta alteração atualiza a importação do declarative_base() no código para utilizar o módulo sqlalchemy.orm ao invés de sqlalchemy.ext.declarative, uma vez que a partir da versão mais recente do SQLAlchemy, a declaração de classes base para mapeamento ORM é feita através do módulo sqlalchemy.orm. Essa atualização é importante para manter a compatibilidade com as novas versões do SQLAlchemy e garantir a manutenção do código a longo prazo.

--- src/database/__init__.py
+++ src/database/__init__.py
@@ -1,8 +1,7 @@
 import os
 
 from sqlalchemy import create_engine
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import declarative_base, sessionmaker
 
 db_user = os.getenv("SK_DB_USER")
 db_pass = os.getenv("SK_DB_PASS")
