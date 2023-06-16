184cd692b7ab8e47eac6dbc3a3efdea7ac7c4087
André Luiz <alcostatorres@gmail.com>
2023-06-16 15:12:54 -0300

    feature: remove customer address if is updating

--- app/database/crud.py
+++ app/database/crud.py
@@ -1,10 +1,8 @@
 from sqlalchemy.orm import Session
-from ..utils import get_logger
 
 from ..models.Models import Customers, Adresses
 from ..dto.Customer import CustomerData
 from ..dto.Address import AddressData
-logger = get_logger(__name__)
 
 def get_customer_by_id(db: Session, customer_id: int):
     return db.query(Customers).filter(Customers.cliente_id == customer_id).first()
@@ -32,16 +30,26 @@ def save_customer(db: Session, customer: CustomerData, address: dict):
 
     return db_customer
 
-def get_address_by_cep(db: Session, cep: str, customer_id: int):
+def get_customer_address_by_cep(db: Session, cep: str, customer_id: int):
     return db.query(Adresses).filter(Adresses.cep == cep, Adresses.cliente_id == customer_id).first()
+
+def get_customer_address(db: Session, customer_id: int):
+    return db.query(Adresses).filter(Adresses.cliente_id == customer_id).first()
 
 def save_customer_address(db: Session, address: AddressData, customer_id: int):
     db_address = Adresses(**address.dict(), cliente_id=customer_id)
-    address = get_address_by_cep(db, db_address.cep, customer_id)
-    if address:
-        return db_address
+    customer_address = get_customer_address(db, customer_id)
+    if customer_address:
+        if customer_address.cep != db_address.cep:
+            delete_customer_address(db, customer_address)
+        else:
+            return db_address
 
     db.add(db_address)
     db.commit()
     db.refresh(db_address)
     return db_address
+
+def delete_customer_address(db: Session, address: dict) -> None:
+    db.delete(address)
+    db.commit()
